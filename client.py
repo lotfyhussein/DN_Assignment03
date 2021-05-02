08f2ecc908faa12540c9223fbd23a2beae78dfa0
Lotfy <lotfyhussein@aucegypt.edu>
2021-05-02 23:40:34 +0200

    Add client/server functionality

---
+++ client.py
@@ -0,0 +1,124 @@
+from socket import *
+import hashlib
+import os
+serverPort = 7700
+serverURL = "localhost"
+BUFFER_SIZE = 1024
+path = './Client_Files'
+
+
+def generate_md5_hash (file_data):
+    md5_hash = hashlib.md5(file_data)
+    f_id = md5_hash.hexdigest()
+    return str(f_id)
+
+def getFileSize(filename):
+    st = os.stat(filename)
+    return st.st_size
+
+
+def handle_list_files(clientSocket):
+    msg = clientSocket.recv(BUFFER_SIZE).decode()
+    if msg == "No files available at the moment":
+        print("Server Response: No files available at the moment")
+        return None
+    rsize = 0
+    size = int(msg)
+    clientSocket.send("start".encode())
+    data = ''
+    while True:
+        data += clientSocket.recv(BUFFER_SIZE).decode()
+        rsize += len(data)
+        if (rsize >= size):
+            print(data)
+            return data.splitlines()
+
+def handle_upload(clientSocket):
+    clientSocket.recv(BUFFER_SIZE).decode()
+    filename = input('Enter filename to be sent: ')
+    file_size = getFileSize(path + '/' + filename)
+    msg = filename + ';' + str(file_size)
+    clientSocket.send(msg.encode())
+    clientSocket.recv(BUFFER_SIZE).decode() # wait for server's "Ready to receive a file" msg
+    ###
+    # start sending the file
+    ###
+    f = open(path + '/' + filename, "rb")
+    data = f.read(1024)
+    while (data):
+       clientSocket.send(data)
+       data = f.read(1024)
+    f.close()
+    print('Sent ({} bytes)'.format(file_size)) 
+
+    ###
+    # MD5 Veirifcation Porcess
+    ###
+    md5_server = clientSocket.recv(BUFFER_SIZE).decode() # get Md5 hash from server
+    f = open(path + '/' + filename, "rb")
+    file_data = f.read()
+    md5_local = generate_md5_hash(file_data)
+    if md5_server != md5_local:
+        print("Fail")
+    else:
+        print("Success!")
+    return
+
+def handle_download(clientSocket, received_file_list):
+    clientSocket.recv(BUFFER_SIZE).decode()
+    file_id = input('Enter file id: ')
+    for f in received_file_list:
+        l = f.split(';')
+        if l[0] == file_id:
+            filename = l[0] + l[1]
+            size = int(l[2])
+    if not size:
+        print('No such file available!')
+        return
+    clientSocket.send(file_id.encode())
+    rsize = 0
+    with open(path + '/' + filename , 'wb') as f:
+        while True:
+            data =  clientSocket.recv(BUFFER_SIZE)
+            rsize += len(data)
+            f.write(data)
+            if (rsize >= size):
+                break
+    f.close()
+    print('Recieved ({} bytes)'.format(size)) 
+    ###
+    # Generate MD5 Hash and compare it to file_id
+    ###
+    f = open(path + '/' + filename, "rb")
+    file_data = f.read()
+    md5_hash = generate_md5_hash(file_data)
+    if md5_hash != file_id:
+        print("Fail")
+    else:
+        print("Success!")
+    return
+
+
+# Create TCP socket for future connections
+#
+clientSocket = socket(AF_INET, SOCK_STREAM)
+# 
+# Connect the client to the specified server
+#
+clientSocket.connect((serverURL, serverPort))
+print("Client connected to server: " + serverURL + ":" + str(serverPort))
+while True:
+    command = input('Enter command: ') 
+    clientSocket.send(command.encode())
+    if command == "LIST_FILES":
+        received_file_list = handle_list_files(clientSocket)
+    elif command == "UPLOAD":
+        handle_upload(clientSocket)
+    elif command == "DOWNLOAD":
+        handle_download(clientSocket, received_file_list)
+    elif command == "DISCONNECT":
+        break
+        clientSocket.close()
+    else:
+        print('Unknown Command')
+
---
+++ server.py
@@ -0,0 +1,115 @@
+from socket import *
+import hashlib
+import os
+path = './Server_Files'
+serverPort = 7700
+serverURL = "localhost"
+BUFFER_SIZE = 1024
+
+def generate_md5_hash (file_data):
+    md5_hash = hashlib.md5(file_data)
+    f_id = md5_hash.hexdigest()
+    return str(f_id)
+
+def getFileList(files):
+    size = 0
+    msg_list = []
+    for f in files:
+        file_size = getFileSize(path + '/' + f)
+        msg = f + ';' + str(file_size)
+        size += len(msg)
+        msg_list.append(msg)
+    return msg_list, size
+
+def getFileSize(filename):
+    st = os.stat(filename)
+    return st.st_size
+
+def handle_list_files(connectSocket):
+    files = os.listdir(path)
+    if len(files) == 0:
+        connectSocket.send(str("No files available at the moment").encode())
+    else:            
+        msg_list, msg_size = getFileList(files)
+        connectSocket.send(str(msg_size).encode())
+        conf = connectSocket.recv(BUFFER_SIZE).decode()
+        for f in msg_list:
+            connectSocket.send((f + '\n').encode())
+    return
+
+def handle_upload(connectSocket):
+    connectSocket.send("Please send file name and file size".encode())
+    msg = connectSocket.recv(BUFFER_SIZE).decode()
+    msg_split = msg.split(";")
+    filename = msg_split[0]
+    size = int(msg_split[1])
+    rsize = 0
+    connectSocket.send("Ready to receive a file".encode())
+    with open(path + '/' + filename, 'wb') as f:
+        while True:
+            data = connectSocket.recv(BUFFER_SIZE)
+            rsize += len(data)
+            f.write(data)
+            if (rsize >= size):
+                break
+    f.close()
+    ###
+    # Generate MD5 Hash and send it to the client
+    ###
+    f = open(path + '/' + filename, "rb")
+    file_data = f.read()
+    md5_hash = generate_md5_hash(file_data)
+    connectSocket.send(md5_hash.encode())
+    os.rename(path + '/' + filename, path + '/' + md5_hash + ';' + filename)
+    print('Successfully received the file ({} bytes)'.format(size)) 
+    return 
+
+def handle_download(connectSocket):
+    connectSocket.send("Please send a file ID".encode())
+    file_id = connectSocket.recv(BUFFER_SIZE).decode()
+    files = os.listdir(path)
+    file_list, _ = getFileList(files)
+    for f in file_list:
+        l = f.split(';')
+        if l[0] == file_id:
+            filename = l[0] + ';' + l[1]
+            size = int(l[2])
+    ###
+    # start sending the file
+    ###
+    f = open(path + '/' + filename, "rb")
+    data = f.read(1024)
+    while (data):
+       connectSocket.send(data)
+       data = f.read(1024)
+    f.close()
+    print('Successfully sent the file ({} bytes)'.format(size)) 
+    return
+
+
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind((serverURL, serverPort))
+serverSocket.listen(1)
+print("Server is listening on port: " + str(serverPort))
+
+while True:
+    # 
+    # Accept incoming client connection
+    #
+    connectSocket, addr = serverSocket.accept()
+    print("Client connected: " + str(addr))
+    while True:
+        data = connectSocket.recv(BUFFER_SIZE).decode()
+        if not data:
+            continue
+        if data == "LIST_FILES":
+            handle_list_files(connectSocket)
+        elif data == "UPLOAD":
+            handle_upload(connectSocket)
+        elif data == "DOWNLOAD":
+            handle_download(connectSocket)
+        elif data == "DISCONNECT":
+            print("Client disconnected")
+            connectSocket.close()
+            break
